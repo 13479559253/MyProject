c810ce2321f1dbb2ac6b49e5cc6830e2728e4be0
Chengjun Cai <2646221507@qq.com>
2026-05-15 14:44:47 +0800

    Delete src directory

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-import router from "./router/index.js";
-import 'element-plus/dist/index.css'
-import ElementPlus from 'element-plus'
-
-createApp(App).use(ElementPlus).use(router).mount('#app')
